4249845b044f468cdb0acd3d7a59610df58c3cd1
antoniogutim <antoniogutim@gmail.com>
2025-05-11 18:23:56 -0600

    1er ejercicio de Java Script tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,17 @@
+console.log(typeof 42); // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof false); // boolean
+
+/// Ejemplos propios
+console.log(typeof [1,2,3]); // object
+console.log(typeof {a: 1 }); // object
+console.log(typeof function(){}); // function
+console.log(typeof NaN); // number
+
+
